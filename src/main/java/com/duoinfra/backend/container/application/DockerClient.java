cb1df0ef87c7727409222d7f8e22f7a070634e76
최혁순 <choehyeogsun@choehyeogsun-ui-MacBookPro.local>
2026-07-13 20:00:28 +0900

    feat: DockerClient 인터페이스에 removeContainer, getContainerMetrics 추가 (#7)

--- src/main/java/com/duoinfra/backend/container/application/DockerClient.java
+++ src/main/java/com/duoinfra/backend/container/application/DockerClient.java
@@ -2,4 +2,6 @@ package com.duoinfra.backend.container.application;
 
 public interface DockerClient {
     DockerProvisionResult provisionContainer(int cpu, int memory);
+    void removeContainer(String containerId);
+    ContainerMetrics getContainerMetrics(String containerId);
 }
